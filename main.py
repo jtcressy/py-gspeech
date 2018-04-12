0da0de6c1a2ed0ae672438767187eb8bd1bb5e49
jtcressy <joel@jtcressy.net>
2018-04-12 11:38:58 -0600

    use flex python3

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return f"Hello, World! TypeOf(int): {type(int)}"
+
 
 if __name__ == '__main__':
     app.run()
